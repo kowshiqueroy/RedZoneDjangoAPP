a853d6680f4749063d98526b27dbfc90718eb182
kowshiqueroy <34647424+kowshiqueroy@users.noreply.github.com>
2021-04-06 01:32:17 +0600

    Update Signup.java

--- RedZone/app/src/main/java/com/example/redzone/Signup.java
+++ RedZone/app/src/main/java/com/example/redzone/Signup.java
@@ -56,45 +56,72 @@ EditText editfn, editln, editdob, editemail, editgender, editprofession, editpas
             public void onClick(View v) {
 
 
+
+
+                Log.d("Signup", "btclick");
+                String gen;
+                if (editgender.getText().toString()=="male" ||editgender.getText().toString()=="Male"){
+
+                    gen="1";
+                } else{gen="0"; }
+
+
+                String hm="{\"first_name\":\"" +
+                        editfn.getText().toString() +
+                        "\",\"last_name\":\"" +
+                        editln.getText().toString()+
+                        "\",\"gender\":" +
+                        gen +
+                        ",\"email\":\"" +
+                        editemail.getText().toString() +
+                        "\",\"profession\":\"" +
+                        editprofession.getText().toString() +
+                        "\",\"username\":\"" +
+                        editemail.getText().toString() +
+                        "\",\"password\":\"" +
+                        editpass.getText().toString() +
+                        "\",\"password2\":\"" +
+                        editpass2.getText().toString() +
+                        "\"}";
+Log.d("Json",hm);
+
+                Log.d("Signup", String.valueOf(hm));
+Call <RedUser> call = RetrofitClient.getInstance().getMyApi().createUser(hm,editdob.getText().toString());
+                Log.d("Signup", "afterbtclick");
+call.enqueue(new Callback<RedUser>() {
+
+    @Override
+    public void onResponse(Call<RedUser> call, Response<RedUser> response) {
+        Log.d("Res", "ok");
+        if (response.body() instanceof RedUser){
+            Log.d("Res", "ok");
+            Log.d("Res", "ok");
+            if(editemail.getText().toString()==response.body().getEmail()){
+                Toast toast=Toast.makeText(getApplicationContext(),"Successful",Toast.LENGTH_SHORT);
+                toast.show();
                 Intent i1= new Intent(Signup.this,Login.class);
                 startActivity(i1);
 
-                Log.d("Signup", "btclick");
-
-
-                String hm="{\"first_name\":\"Kowshique\",\"last_name\":\"Roy\",\"gender\":1,\"email\":\"Niger@gmail.com\",\"profession\":\"Student\",\"username\":\"Meher\",\"password\":\"k12345678M\",\"password2\":\"k12345678M\"}";
-
-
-                Log.d("Signup", String.valueOf(hm));
-Call <RedUser> call = RetrofitClient.getInstance().getMyApi().createUser(hm,"1997-11-01");
-                Log.d("Signup", "afterbtclick");
-call.enqueue(new Callback<RedUser>() {
-    @Override
-    public void onResponse(Call<RedUser> call, Response<RedUser> response) {
-        Log.d("Signup", response.body().getEmail());
+            }
+            Toast toast=Toast.makeText(getApplicationContext(),"Failed",Toast.LENGTH_SHORT);
+            toast.show();
+            Log.d("Res", "if not");
+        }
+        Toast toast=Toast.makeText(getApplicationContext(),"Failed",Toast.LENGTH_SHORT);
+        toast.show();
+        Log.d("Res", "if not");
     }
 
     @Override
     public void onFailure(Call<RedUser> call, Throwable t) {
-        Log.d("Signup", "Json failed");
+        Toast toast=Toast.makeText(getApplicationContext(),"Failed",Toast.LENGTH_SHORT);
+        toast.show();
+        Log.d("Res", "failed");
+
     }
 });
-                //Intent i1= new Intent(Signup.this,Login.class);
-                //startActivity(i1);
-
             }
         });
 
-
-
-
-
-
     }
-
-
-
-
-
-
 }
