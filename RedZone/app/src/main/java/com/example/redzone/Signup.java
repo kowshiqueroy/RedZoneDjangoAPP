70a372295a31e9f7b60d6049165429e9c5925fb4
kowshiqueroy <34647424+kowshiqueroy@users.noreply.github.com>
2021-04-06 01:04:13 +0600

    Update Signup.java

--- RedZone/app/src/main/java/com/example/redzone/Signup.java
+++ RedZone/app/src/main/java/com/example/redzone/Signup.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.HashMap;
@@ -19,6 +20,7 @@ import retrofit2.Response;
 
 public class Signup extends AppCompatActivity {
 Button loginbtn,signupbtn2;
+EditText editfn, editln, editdob, editemail, editgender, editprofession, editpass, editpass2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +30,15 @@ Button loginbtn,signupbtn2;
 
         loginbtn= (Button) findViewById(R.id.loginbtn2);
         signupbtn2= (Button) findViewById(R.id.signupbtnsave);
+
+        editfn= (EditText) findViewById(R.id.editfn);
+        editln= (EditText) findViewById(R.id.editln);
+        editdob= (EditText) findViewById(R.id.editdob);
+        editemail= (EditText) findViewById(R.id.editemail);
+        editgender= (EditText) findViewById(R.id.editgender);
+        editprofession= (EditText) findViewById(R.id.editprofession);
+        editpass= (EditText) findViewById(R.id.editpass);
+        editpass2= (EditText) findViewById(R.id.editpass2);
 
 
 
@@ -50,15 +61,6 @@ Button loginbtn,signupbtn2;
 
                 Log.d("Signup", "btclick");
 
-               /* HashMap<String, Object> hm = new HashMap<String, Object>();
-                hm.put("first_name","Kowshique");
-                hm.put("last_name","Roy");
-                hm.put("gender",1);
-                hm.put("email","Kowshique@gmail.com");
-                hm.put("profession","Student");
-                hm.put("username","Kowshiqueroy");
-                hm.put("password1","k12345678M");
-                hm.put("password2","k12345678M");*/
 
                 String hm="{\"first_name\":\"Kowshique\",\"last_name\":\"Roy\",\"gender\":1,\"email\":\"Niger@gmail.com\",\"profession\":\"Student\",\"username\":\"Meher\",\"password\":\"k12345678M\",\"password2\":\"k12345678M\"}";
 
